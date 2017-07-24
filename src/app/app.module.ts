af4b51aa2595bbe1376b90827f057c9b0985a2c1
Harsh Kumar Lamba <hlamba@sapient.com>
2017-07-24 23:40:00 +0530

    Materialize css and angular-materialize added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { MaterializeModule } from 'ng2-materialize';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -8,6 +10,7 @@ import { BannerComponent } from './banner/banner.component';
 import { LoginComponent } from './shared/login/login.component';
 import { CardComponent } from './shared/card/card.component';
 import { ProductsComponent } from './products/products.component';
+import { NavItemComponent } from './shared/nav-item/nav-item.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,13 @@ import { ProductsComponent } from './products/products.component';
     BannerComponent,
     LoginComponent,
     CardComponent,
-    ProductsComponent
+    ProductsComponent,
+    NavItemComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule,
+    MaterializeModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
