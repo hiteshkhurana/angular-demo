3ecf9155fa10eb8d8a93e89f5cb08651466b77e5
Harsh Kumar Lamba <hlamba@sapient.com>
2017-08-06 13:49:04 +0530

    login click handler

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from '../../service/authentication/authentication.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.service.logout();
   }
 
 }
